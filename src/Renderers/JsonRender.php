f9172c62c32cee36bb3f3ddaa2522e05b1c8306a
itohin <itoxa@MacBook-Pro.local>
2019-03-03 15:53:35 +0300

    6 step refactor

--- src/Renderers/JsonRender.php
+++ src/Renderers/JsonRender.php
@@ -4,18 +4,5 @@ namespace Gendiff\Renderers;
 
 function renderJson($ast)
 {
-    $filteredAst = astFilter($ast);
-    return json_encode($filteredAst, JSON_PRETTY_PRINT + JSON_NUMERIC_CHECK);
-}
-
-function astFilter($ast)
-{
-    $ast = array_filter($ast);
-    foreach ($ast as &$node) {
-        if (is_array($node)) {
-            $node = astFilter($node);
-        }
-    }
-
-    return $ast;
+    return json_encode($ast, JSON_PRETTY_PRINT + JSON_NUMERIC_CHECK);
 }
